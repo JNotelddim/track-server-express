8d61f591879533785dab8228d00bf2f137296ab6
Jared Middleton <jaredmiddleton3.14@gmail.com>
2020-01-10 11:48:53 -0800

    Fix User.comparePassword typo - set up basic Track model

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require("./models/User");
+require("./models/Track");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -6,11 +7,13 @@ const credentials = require("./db.hide");
 const { email, password } = credentials;
 
 const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
 const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri =
   "mongodb+srv://" +
