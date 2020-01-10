ebc6d254d056bacd87d389b684ea653b92cd0e73
Jared Middleton <jaredmiddleton3.14@gmail.com>
2020-01-09 17:42:42 -0800

    Set up basic /signup endpoint which creates user in mongodb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
+require("./models/User");
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const credentials = require("./db.hide");
+const { email, password } = credentials;
+
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri =
+  "mongodb+srv://" +
+  email +
+  ":" +
+  password +
+  "@cluster0-apuvb.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", err => {
+  console.error("Error connecting to mongo", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Hi there!");
